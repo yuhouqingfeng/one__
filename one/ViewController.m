58056fac6c4e162cd448e7bc68d0bf4b3eadb12a
wuxinyi <15091861910@163.com>
2017-11-26 19:39:58 +0800

    Initial Commit

---
+++ one/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  one
+//
+//  Created by wuxinyi on 17/11/26.
+//  Copyright © 2017年 wuxinyi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ one/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  one
+//
+//  Created by wuxinyi on 17/11/26.
+//  Copyright © 2017年 wuxinyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
