948f8d99b6f8f931c2ff05926487495d6bdf789d
wuxinyi <15091861910@163.com>
2018-03-17 15:51:00 +0800

    third

---
+++ group/OneView.h
@@ -0,0 +1,13 @@
+//
+//  OneView.h
+//  one
+//
+//  Created by wuxinyi on 18/3/13.
+//  Copyright © 2018年 wuxinyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OneView : UIView
+
+@end
---
+++ group/OneView.m
@@ -0,0 +1,21 @@
+//
+//  OneView.m
+//  one
+//
+//  Created by wuxinyi on 18/3/13.
+//  Copyright © 2018年 wuxinyi. All rights reserved.
+//
+
+#import "OneView.h"
+
+@implementation OneView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
